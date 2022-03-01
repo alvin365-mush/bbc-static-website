4ef0b23ba99a8f852b3fa1ec1da8a9eb04db441e
al <alvinmusiega@gmail.com>
2022-03-01 11:19:42 +0300

    tailwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-pattern": "url('./images/SMOG_vdyw4.jpg')",
+        "hero-pattern": "url('/images/SMOG_vdyw4.jpg')",
       },
     },
   },
